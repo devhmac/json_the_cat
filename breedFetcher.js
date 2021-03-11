49da79028be5bd885d4adf375abb32c3c91170c6
Devin MacGillivray <devhmac@gmail.com>
2021-03-11 21:01:23 +0000

    created index to house the get request, refactored breedFetcher to be a single function

--- breedFetcher.js
+++ breedFetcher.js
@@ -3,22 +3,35 @@ const request = require('request');
 
 const breedInput = cliArg[0];
 
-request('https://api.thecatapi.com/v1/breeds/search?q=' + breedInput, (error, response, body) => {
-  //console.log(typeof body) // body is a string, because its a JSON STRING!!!!!
-  if (error) {
-    console.log(error);
-    return;
-  }
-  if (response.statusCode !== 200) {
-    console.log(`your Status code was ${response.statusCode} the search was not completed, there may be something wrong with the site`);
-    return;
-  }
-  const returnData = JSON.parse(body); //it is now an object in an array
-  if (returnData.length === 0) {
-    console.log(`Your search ${breedInput} did not return any results`);
-    return;
-  }
-  console.log(returnData[0]['description']); //returning the description key value from the first element in the returnData array
-  //console.log(typeof returnData) // is now an object after the JSON.parse();
+// const breedDescription = fetchBreedDescription('siberian', (error, description) => {
 
-});
+
+
+// });
+
+const fetchBreedDescription = (breedName, callback) => {
+
+  request('https://api.thecatapi.com/v1/breeds/search?q=' + breedName, (error, response, body) => {
+    //console.log(typeof body) // body is a string, because its a JSON STRING!!!!!
+    if (error) {
+      callback(error);
+      return;
+    }
+    if (response.statusCode !== 200) {
+      console.log(`your Status code was ${response.statusCode} the search was not completed, there may be something wrong with the site`);
+      callback(error);
+      return;
+    }
+    const returnData = JSON.parse(body); //it is now an object in an array
+    if (returnData.length === 0) {
+      console.log(`Your search ${breedInput} did not return any results`);
+      callback(error);
+      return;
+    }
+    return returnData[0]['description']; //returning the description key value from the first element in the returnData array
+    //console.log(typeof returnData) // is now an object after the JSON.parse();
+
+  })
+};
+
+module.exports = { fetchBreedDescription }
